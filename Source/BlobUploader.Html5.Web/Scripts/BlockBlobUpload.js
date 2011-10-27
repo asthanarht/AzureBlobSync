33c28d4f6f131a61a283359ef553c69f9b9a750e
RahulRai_cp <SND\RahulRai_cp>
2011-10-27 11:50:51 +0000

    Prgress bar activated. Final Draft.

--- Source/BlobUploader.Html5.Web/Scripts/BlockBlobUpload.js
+++ Source/BlobUploader.Html5.Web/Scripts/BlockBlobUpload.js
@@ -1,69 +1,101 @@
-﻿var blockCounter = 0;
-var totalNumberOfBlocks = 0;
-var message = '';
+﻿var file = null;
+var fileControl = null;
+var statusLabel = null;
+var progressElement = null;
+var uploadButton = null;
+var cancelButton = null;
+var jqxhr = null;
+this.totalBlocks = 0;
 
-function startUpload(fileElementId, blockLength, uploadProgressElement, statusLabel) {
-    var file = document.getElementById(fileElementId).files[0];
-    var statusLabel = document.getElementById(statusLabel);
-    var progressElement = document.getElementById(uploadProgressElement);
-    statusLabel.innerHTML = '';
-    if (file.size <= 0) {
-        statusLabel.innerHTML = 'Please select a file!';
-        return;
+function cancelUpload() {
+    if (this.jqxhr != null) {
+        this.jqxhr.abort();
     }
+}
 
-    this.totalNumberOfBlocks = Math.ceil(file.size / blockLength);
-    this.blockCounter = 0;
-    this.message = '';
-    progressElement.removeAttribute('hidden');
-    $.ajax({
-        type: "POST",
-        async: true,
-        url: "/Home/PrepareMetaData",
-        data: { "blocksCount": totalNumberOfBlocks, "fileName": file.name, "fileSize": file.size },
-        dataType: "json",
-        error: function () {
-            statusLabel.innerHTML = 'Failed to send file meta data';
-            progressElement.setAttribute('hidden', 'hidden');
-        },
-        success: function () {
-            var start = 0;
-            var end = Math.min(blockLength, file.size) - 1;
-            var incrimentalIdentifier = 1;
-            while (start <= file.size - 1) {
-                var data = new FormData();
-                data.append(incrimentalIdentifier, file.webkitSlice(start, end));
-                $.ajax({
-                    async: true,
-                    url: '/Home/UploadBlock',
-                    data: data,
-                    cache: false,
-                    contentType: false,
-                    processData: false,
-                    type: 'POST',
-                    error: function (notice) {
-                        statusLabel.innerHTML = notice;
-                        progressElement.setAttribute('hidden', 'hidden');
-                        return false;
-                    },
-                    success: function (notice) {
-                        blockCounter += 1;
-                        if (notice.error || notice.isLastBlock) {
-                            this.message = notice.message;
-                        }
-
-                        if (notice.error || blockCounter == totalNumberOfBlocks) {
-                            statusLabel.innerHTML = this.message;
-                            progressElement.setAttribute('hidden', 'hidden');
-                            return false;
-                        }
-                    }
-                });
+var sendFile = function (blockLength) {
+    var start = 0, end = Math.min(blockLength, file.size) - 1, incrimentalIdentifier = 1, sendNextChunk, fileChunk = null;
+    this.statusLabel.innerHTML = '';
+    sendNextChunk = function () {
+        fileChunk = new FormData();
+        fileChunk.append(incrimentalIdentifier, file.webkitSlice(start, end));
+        this.progressElement.setAttribute('value', Math.floor((incrimentalIdentifier - 1) * 100 / this.totalBlocks).toString());
+        this.jqxhr = $.ajax({
+            async: true,
+            url: '/Home/UploadBlock',
+            context: this,
+            data: fileChunk,
+            cache: false,
+            contentType: false,
+            processData: false,
+            type: 'POST',
+            error: function (request, error) {
+                this.statusLabel.innerHTML = error == 'abort' ? 'File upload has been cancelled.' : 'The file could not be uploaded because ' + error;
+                this.fileControl.value = '';
+                resetControls();
+                return;
+            },
+            success: function (notice) {
+                if (notice.error || notice.isLastBlock) {
+                    this.statusLabel.innerHTML = notice.message;
+                    this.fileControl.value = '';
+                    resetControls();
+                    return;
+                }
 
                 start = end + 1;
                 end = Math.min(start + blockLength, file.size) - 1;
-                incrimentalIdentifier++;
+                ++incrimentalIdentifier;
+                sendNextChunk();
             }
-        }
+        });
+    };
+
+    sendNextChunk();
+};
+
+function startUpload(fileElementId, blockLength, uploadProgressElement, statusLabel, uploadButton, cancelButton) {
+    this.file = document.getElementById(fileElementId).files[0];
+    this.fileControl = document.getElementById(fileElementId);
+    this.statusLabel = document.getElementById(statusLabel);
+    this.progressElement = document.getElementById(uploadProgressElement);
+    this.uploadButton = document.getElementById(uploadButton); ;
+    this.cancelButton = document.getElementById(cancelButton); ;
+    this.blockLength = blockLength;
+
+    this.statusLabel.innerHTML = '';
+    this.uploadButton.setAttribute('disabled', 'disabled');
+    this.fileControl.setAttribute('disabled', 'disabled');
+    this.cancelButton.removeAttribute('disabled');
+    if (this.file == null || this.file.size <= 0) {
+        this.statusLabel.innerHTML = 'Please select a file.';
+        resetControls();
+        return;
+    }
+
+    this.totalBlocks = Math.ceil(file.size / blockLength);
+    this.progressElement.removeAttribute('hidden');
+    this.progressElement.setAttribute('value', '0');
+    this.statusLabel.innerHTML = 'Sending file metadata to server.';
+    $.ajax({
+        type: "POST",
+        context: this,
+        async: true,
+        url: '/Home/PrepareMetaData',
+        data: { 'blocksCount': this.totalBlocks, 'fileName': this.file.name, 'fileSize': this.file.size },
+        dataType: "json",
+        error: function () {
+            this.statusLabel.innerHTML = 'Failed to send file meta data. Retry after some time.';
+            this.fileControl.value = '';
+            resetControls();
+        },
+        success: sendFile(blockLength)
     });
+}
+
+function resetControls() {
+    this.progressElement.setAttribute('hidden', 'hidden');
+    this.cancelButton.setAttribute('disabled', 'disabled');
+    this.fileControl.removeAttribute('disabled');
+    this.uploadButton.removeAttribute('disabled');
 }
